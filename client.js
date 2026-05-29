a3db0bd144cf6ca5b0c0e93431129011a43083c8
YoungWoo <102403336+YoungWoo-Ji@users.noreply.github.com>
2026-05-29 10:21:55 +0900

    인텐트 수정

--- client.js
+++ client.js
@@ -4,7 +4,6 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMembers
   ],
   partials: [
